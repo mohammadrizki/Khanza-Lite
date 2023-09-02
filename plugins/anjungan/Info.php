0ad2ca098094759678209f524f516a4fe160e7e1
Mohammad Rizki <mohammadrizki.md@gmail.com>
2023-09-02 16:22:12 +0800

    Update from upstream

--- plugins/anjungan/Info.php
+++ plugins/anjungan/Info.php
@@ -6,7 +6,7 @@ return [
     'version'       =>  '1.0',
     'compatibility' =>  '2023',
     'icon'          =>  'desktop',
-    'pages'            =>  ['Anjungan Pasien Mandiri' => 'anjungan'],
+    'pages'         =>  ['Anjungan Pasien Mandiri' => 'anjungan'],
     'install'       =>  function () use ($core) {
       $core->db()->pdo()->exec("INSERT INTO `mlite_settings` (`module`, `field`, `value`) VALUES ('anjungan', 'display_poli', '')");
       $core->db()->pdo()->exec("INSERT INTO `mlite_settings` (`module`, `field`, `value`) VALUES ('anjungan', 'carabayar', '')");
